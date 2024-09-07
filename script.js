9bd98ed68f39de8976f2117f9fec89f85536aca6
dev-gursewak <gursewak@duck.com>
2024-09-07 00:57:26 -0600

    Created playRound and PlayGame function created logic for deciding a winner or match is tied will be working on how to recall a function with a new values.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello World")
-
 function getComputerChoice(){
     let randomNumber = Math.random()*3;
     if (randomNumber <= 1){
@@ -12,15 +10,54 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let humanChoice = prompt("input your choice from rock, paper, scissor", "rock");
-    humanChoice = humanChoice.toLowerCase();
-    switch (humanChoice){
+    let humanInput = prompt("input your choice from rock, paper, scissor", "rock");
+    humanInput = humanInput.toLowerCase();
+    switch (humanInput){
         case("rock" || "paper" || "scissor"):
-        return humanChoice;
+        return humanInput;
         break;
         default:
             return "WORNG INPUT! Please select from rock, paper and scissor"
     }   
 }
 
-console.log(getHumanChoice())
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === "rock" && computerChoice === "paper"){
+        computerScore++;
+        return "YOU LOSE! Paper beats rock";
+    } else if (humanChoice === "rock" && computerChoice === "scissor"){
+        humanScore++;
+        return "YOU WON! rock beats scissor";
+    } else if (humanChoice === "paper" && computerChoice === "rock"){
+        humanScore++;
+        return "YOU WON! paper beats rock";
+    } else if (humanChoice === "paper" && computerChoice === "scissor"){
+        computerScore++;
+        return "YOU LOSE! scissor beats paper";
+    } else if (humanChoice === "scissor" && computerChoice === "rock"){
+        computerScore++;
+        return "YOU LOSE! rock beats scissor";
+    } else if (humanChoice === "scissor" && computerChoice === "paper"){
+        humanScore++
+        return "YOU WON! scissor beats paper";
+    } else {
+        return "NO RESULT! Play another match"
+    }
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+function playGame(){
+    playRound(humanSelection, computerSelection);
+    playRound(humanSelection, computerSelection);
+    playRound(humanSelection, computerSelection);
+    playRound(humanSelection, computerSelection);
+    playRound(humanSelection, computerSelection);
+    return `You Score is ${humanScore} \n Computer Score is ${computerScore}`
+}
+
+console.log(playGame())
